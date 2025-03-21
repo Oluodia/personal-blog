e91391f52e9a5b281f29e7bfefd29ecdb9858c35
Oluodia <123239434+Oluodia@users.noreply.github.com>
2025-03-21 23:51:23 +0300

    Added pagination

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,13 @@
 @php
 	use App\Models\Post;
-	use App\Models\User;
 	use Illuminate\Support\Str;
+
+	$posts = Post::paginate(4);
 @endphp
 
 <x-app-layout>
 	<div class="grid grid-cols-1 md:grid-cols-1 gap-10">
-		<!-- Post example -->
-		<!-- <article class="bg-white rounded-lg shadow-md p-6 mb-8">
-				<h2 class="text-xl font-bold text-indigo-600">Первый пост</h2>
-				<p class="text-gray-600 mt-2"><span class="font-semibold">12 октября 2023</span> | <span>Автор</span>
-				</p>
-				<p class="mt-4">Это содержание моего первого поста. Здесь будет много интересного и полезного.</p>
-				<a href="#" class="text-indigo-600 hover:underline mt-4 inline-block">Читать далее</a>
-			</article> -->
-		@foreach (Post::all() as $post)
+		@foreach ($posts as $post)
 			<section class="bg-white rounded-lg shadow-md p-6 mb-8">
 				<h2 class="text-xl font-bold text-indigo-600">{{ Str::limit($post->title, 50) }}</h2>
 
@@ -29,4 +22,5 @@
 			</section>
 		@endforeach
 	</div>
+	{{ $posts->links() }}
 </x-app-layout>
